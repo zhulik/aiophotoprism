24be8152d37f3f096f13d69ec76f39848a9ab0c7
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-03-06 18:47:19 +0100

    pylint

--- tests/test_photoprism.py
+++ tests/test_photoprism.py
@@ -1,10 +1,11 @@
 """Tests for Photoprism."""
 
 import pytest
+from aiophotoprism import Photoprism
 from expects import be_a, expect
 
-from aiophotoprism import Photoprism
 
 @pytest.mark.asyncio
 async def test_initialization():
+    """Test initialization."""
     expect(Photoprism("username", "password")).to(be_a(Photoprism))
