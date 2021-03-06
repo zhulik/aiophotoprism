a333f59dbfde0bdadbe1c7f84031ea46c1a44c59
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-03-06 18:45:29 +0100

    Add dummy test

--- tests/test_photoprism.py
+++ tests/test_photoprism.py
@@ -1,5 +1,10 @@
 """Tests for Photoprism."""
 
-# import aiophotoprism
-# import pytest
-# from expects import be_a, expect
+import pytest
+from expects import be_a, expect
+
+from aiophotoprism import Photoprism
+
+@pytest.mark.asyncio
+async def test_initialization():
+    expect(Photoprism("username", "password")).to(be_a(Photoprism))
